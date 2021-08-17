4b251b76b0493369825075839c2ec66cb77d1357
Jennifer Law <jenniferlaw@Jennifers-MacBook-Air-2.local>
2021-08-17 09:21:04 -0700

    commented out tests

--- eqArrays.js
+++ eqArrays.js
@@ -17,10 +17,10 @@ let eqArrays = (array1, array2) => {
   return true;
 };
 
-eqArrays([1, 2, 3], [1, 2, 3]); // => true
-eqArrays([1, 2, 3], [3, 2, 1]); // => false
-
-eqArrays(["1", "2", "3"], ["1", "2", "3"]); // => true
-eqArrays(["1", "2", "3"], ["1", "2", 3]); // => false
-
-assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true); // => should PASS
+//eqArrays([1, 2, 3], [1, 2, 3]); // => true
+//eqArrays([1, 2, 3], [3, 2, 1]); // => false
+//
+//eqArrays(["1", "2", "3"], ["1", "2", "3"]); // => true
+//eqArrays(["1", "2", "3"], ["1", "2", 3]); // => false
+//
+//assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true); // => should PASS
