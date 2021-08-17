81392557744ccaff2f067aadb0039c7d6f176ed5
Jennifer Law <jenniferlaw@Jennifers-MacBook-Air-2.local>
2021-08-17 09:14:38 -0700

    check if 2 arrays are equal to each other

---
+++ eqArrays.js
@@ -0,0 +1,26 @@
+const assertEqual = function (actual, expected) {
+  if (actual === expected) {
+    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`❌❌❌ Assertion Failed:${actual} !== ${expected}`);
+  }
+};
+
+let eqArrays = (array1, array2) => {
+  i = 0;
+  for (let a of array1) {
+    if (a !== array2[i]) {
+      return false;
+    }
+    i++;
+  }
+  return true;
+};
+
+eqArrays([1, 2, 3], [1, 2, 3]); // => true
+eqArrays([1, 2, 3], [3, 2, 1]); // => false
+
+eqArrays(["1", "2", "3"], ["1", "2", "3"]); // => true
+eqArrays(["1", "2", "3"], ["1", "2", 3]); // => false
+
+assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true); // => should PASS
