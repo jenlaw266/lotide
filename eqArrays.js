00d45ab42ffdaf57159d76e3a388fecf08f9d870
Jennifer Law <jenniferlaw@Jennifers-MacBook-Air-2.local>
2021-08-23 14:05:58 -0700

    added eqArraysTest.js

--- eqArrays.js
+++ eqArrays.js
@@ -1,11 +1,3 @@
-const assertEqual = function (actual, expected) {
-  if (actual === expected) {
-    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
-  } else {
-    console.log(`❌❌❌ Assertion Failed:${actual} !== ${expected}`);
-  }
-};
-
 const eqArrays = (array1, array2) => {
   if (array1.length !== array2.length) {
     return false;
@@ -27,10 +19,4 @@ const eqArrays = (array1, array2) => {
   return true;
 };
 
-//eqArrays([1, 2, 3], [1, 2, 3]); // => true
-//eqArrays([1, 2, 3], [3, 2, 1]); // => false
-//
-//eqArrays(["1", "2", "3"], ["1", "2", "3"]); // => true
-//eqArrays(["1", "2", "3"], ["1", "2", 3]); // => false
-//
-//assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true); // => should PASS
+module.exports = eqArrays;
