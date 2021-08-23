0e7a020b45e599de1bbef3e5fbfb9052d80269c7
Jennifer Law <jenniferlaw@Jennifers-MacBook-Air-2.local>
2021-08-23 12:02:35 -0700

    added export & require for assertEqual & head file

---
+++ test/assertEqualTest.js
@@ -0,0 +1,8 @@
+const assertEqual = require('../assertEqual');
+
+// TEST CODE
+ assertEqual("Lighthouse Labs", "Bootcamp");
+ assertEqual(1, 1);
+
+//Assertion Failed: Lighthouse Labs !== Bootcamp
+//Assertion Passed: 1 === 1
